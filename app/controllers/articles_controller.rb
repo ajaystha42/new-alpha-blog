4bfa24e38f0102828527b7db80eb8c2274705cbb
Ajay Shrestha <er.ajaystha@gmail.com>
2022-10-28 16:12:12 +0545

    table view with editing feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController 
+    #for template
     def index
         @articles = Article.all
     end
 
+    #for template
     def new 
         @article = Article.new
     end
@@ -10,15 +12,32 @@ class ArticlesController < ApplicationController
     def create 
         @article = Article.new(article_params)
         if @article.save
-            flash[:notice] = "Article created successfully."
-            redirect_to article_path(@article)
+            flash[:notice] = "Article #{@article.id} created successfully."
+            redirect_to articles_path
+            # redirect_to article_path(@article)
         else
             render 'new'
         end
     end
 
+    #for template
     def show
         @article = Article.find(params[:id])
+    end
+
+    #for template
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article #{@article.id} updated successfully."
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
     end
 
     private
